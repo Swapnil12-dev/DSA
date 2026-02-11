9da5d0eeaf491e5de011db32a24f2d6224c9ab53
Swapnil12-dev <swapnil.ajabe1212@gmail.com>
2026-02-11 10:43:48 +0530

    Time: 11 ms (28.76%) | Memory: 13.7 MB (54.36%) - LeetSync

---
+++ 1128-remove-all-adjacent-duplicates-in-string/remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+     string ans ="";
+     int i=0 ;
+
+     while(i<s.length()){
+
+
+            if((!ans.empty()) && ans.back() == s[i])
+                ans.pop_back();
+            else
+                ans.push_back(s[i]);
+     i++;
+     }
+  
+     return ans ; 
+
+
+    }
+};
