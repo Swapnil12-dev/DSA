645305d86b5149b2c1b1e3510bb224365ad725ff
Swapnil12-dev <swapnil.ajabe1212@gmail.com>
2026-02-11 11:18:27 +0530

    Time: 10 ms (13.78%) | Memory: 26.4 MB (47.52%) - LeetSync

---
+++ 680-valid-palindrome-ii/valid-palindrome-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+
+    bool check(string s , int i  , int j )
+    {
+        while(i<= j ){
+            
+           if(s[i] != s[j])
+                return false;
+        
+            i++;
+            j--;
+ 
+        }
+    return true;
+    }
+    
+
+    bool validPalindrome(string s) {
+        int i =0;
+        int j = s.length() -1 ;
+
+        while(i<=j){
+             if(s[i]==s[j]){
+                i++;
+                j--;
+             }
+            else
+                return check(s,i,j-1) || check(s,i+1 , j);
+        }
+        return true;
+    }
+};
